61eb75acdeaafe421ec553612e31f50420c4ac12
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-05-09 00:46:51 -0300

    create container

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'http://agrocv-api.dbgdang6hvgmbef2.eastus.azurecontainer.io:5000',
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
+  server: {
+    host: true,
+    https: false
   }
 })
